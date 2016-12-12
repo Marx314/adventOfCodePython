2778f1657c0d9cf654e64f7c43f64ad5da0d78ca
Marc Aubry <maubry@internap.com>
2016-12-11 22:29:41 -0500

    Day 11

---
+++ src/Year2016/Day11.py
@@ -0,0 +1,97 @@
+import itertools
+from queue import PriorityQueue
+import copy
+
+__author__ = 'maubry'
+
+
+class Day11(object):
+    def heuristic(self, new_floor_plan, current, cost_so_far):
+        return (len(new_floor_plan._[3]['g']) + len(new_floor_plan._[3]['m'])) * -2
+
+    def a_star_search(self, floor_plan):
+        # A* from http://www.redblobgames.com/pathfinding/a-star/implementation.html#python-astar
+        frontier = PriorityQueue()
+        floor = 0
+        current = (floor, floor_plan)
+        frontier.put((0, current))
+        came_from = {current: None}
+        cost_so_far = {current: 0}
+        current = self.find_solution(came_from, cost_so_far, frontier, 1)
+
+        return cost_so_far, came_from, current
+
+    def find_solution(self, came_from, cost_so_far, frontier, response_count):
+        directions = {0: [1], 1: [-1, 1], 2: [-1, 1], 3: [-1]}
+        solutions = []
+
+        while not frontier.empty():
+            _, current = frontier.get()
+            floor, floor_plan = current
+            if floor == 3 and repr(floor_plan).startswith('[][][][][][]'):
+                print(cost_so_far[current])
+                solutions.append(current)
+                if len(solutions) == response_count:
+                    break
+
+            moves = self.generate_move(floor, floor_plan, 2) + self.generate_move(floor, floor_plan, 1)
+            for move in moves:
+                for direction in directions[floor]:
+                    new_floor_plan = self.apply_move(floor, direction, floor_plan, move)
+                    if not self.valid_floor(new_floor_plan._[floor]):
+                        continue
+                    if not self.valid_floor(new_floor_plan._[floor + direction]):
+                        continue
+                    new_cost = cost_so_far[current] + 1
+                    next = (floor + direction, new_floor_plan)
+                    if next not in cost_so_far or new_cost < cost_so_far[next]:
+                        cost_so_far[next] = new_cost
+                        priority = new_cost + self.heuristic(new_floor_plan, current, cost_so_far)
+                        frontier.put((priority, next))
+                        came_from[next] = current
+        return current
+
+    def generate_move(self, floor, floor_plan, count=2):
+        return list(itertools.combinations(floor_plan._[floor]['g'] + floor_plan._[floor]['m'], count))
+
+    def apply_move(self, current_floor, direction, floor_plan, moves):
+        new_floor_plan = FloorPlan(copy.deepcopy(floor_plan._))
+        for move in moves:
+            if move in new_floor_plan._[current_floor]['g']:
+                self.move(move, new_floor_plan._, current_floor, 'g', direction)
+            elif move in new_floor_plan._[current_floor]['m']:
+                self.move(move, new_floor_plan._, current_floor, 'm', direction)
+        return new_floor_plan
+
+    def move(self, item, floor_plan, floor, item_type, direction):
+        if item in floor_plan[floor][item_type]:
+            floor_plan[floor][item_type].remove(item)
+        floor_plan[floor + direction][item_type].append(item)
+
+    def valid_floor(self, floor):
+        generators = sorted(floor['g'])
+        microchips = sorted(floor['m'])
+        no_generator = len(generators) == 0
+
+        return generators == microchips or no_generator or all(m in generators for m in microchips)
+
+
+class FloorPlan(object):
+    def __init__(self, floor_plan):
+        self._ = floor_plan
+
+    def __hash__(self):
+        return self.__repr__().__hash__()
+
+    def __lt__(self, other):
+        return repr(self) > repr(other)
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def __repr__(self):
+        plan = ''
+        for floor in self._:
+            plan += '{}'.format(sorted(floor['g']))
+            plan += '{}'.format(sorted(floor['m']))
+        return plan
---
+++ test/Year2016/day11_Test.py
@@ -0,0 +1,66 @@
+from unittest import TestCase
+
+import re
+from src import split_data
+from src.Year2016.Day11 import Day11, FloorPlan
+
+
+class Day11Test(TestCase):
+    def setUp(self):
+        self.day = Day11()
+
+    def test_base(self):
+        instructions = '''The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+The second floor contains a hydrogen generator.
+The third floor contains a lithium generator.
+The fourth floor contains nothing relevant.'''
+        floor_plan = self.generate_initial(instructions)
+        cost_so_far, came_from, current = self.day.a_star_search(floor_plan)
+        self.assertEqual(cost_so_far[current], 11)
+
+    def test_puzzle(self):
+        floor_plan = self.generate_initial(self.puzzle())
+        cost_so_far, came_from, current = self.day.a_star_search(floor_plan)
+        states = self.build_state(came_from, cost_so_far, current)
+        self.assertEqual(len(states), 31)
+
+    def test_puzzle_part2(self):
+        floor_plan = self.generate_initial(self.puzzle())
+        floor_plan._[0]['g'].append(1)
+        floor_plan._[0]['g'].append(2)
+        floor_plan._[0]['m'].append(1)
+        floor_plan._[0]['m'].append(2)
+        cost_so_far, came_from, current = self.day.a_star_search(floor_plan)
+        states = self.build_state(came_from, cost_so_far, current)
+        self.assertEqual(len(states), 55)
+
+    def build_state(self, came_from, cost_so_far, current):
+        index = cost_so_far[current]
+        path = {index: came_from[current]}
+        while index > 0:
+            current = came_from[current]
+            path[index] = current
+            index -= 1
+
+        return list(path.values())
+
+    @split_data
+    def generate_initial(self, instructions):
+        floor_plan_list = [{'m': [], 'g': []} for _ in range(4)]
+        element = {'hydrogen': 1, 'lithium': 2, 'thulium': 3, 'plutonium': 4, 'strontium': 5, 'promethium': 6,
+                   'ruthenium': 7}
+        for floor, instruction in enumerate(instructions):
+            result = re.findall(r'((\w*\-?)*\s(microchip|generator))', instruction)
+            for composant in result:
+                if composant[2] == 'microchip':
+                    floor_plan_list[floor]['m'].append(element[re.findall('\w*', composant[0])[0]])
+                elif composant[2] == 'generator':
+                    floor_plan_list[floor]['g'].append(element[re.findall('\w*', composant[0])[0]])
+
+        return FloorPlan(floor_plan_list)
+
+    def puzzle(self):
+        return '''The first floor contains a thulium generator, a thulium-compatible microchip, a plutonium generator, and a strontium generator.
+The second floor contains a plutonium-compatible microchip and a strontium-compatible microchip.
+The third floor contains a promethium generator, a promethium-compatible microchip, a ruthenium generator, and a ruthenium-compatible microchip.
+The fourth floor contains nothing relevant.'''
