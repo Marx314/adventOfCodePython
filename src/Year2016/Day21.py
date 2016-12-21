cfedb7b2cc6cfe59a735326a20a67f02cfa0c316
Marc Aubry <maubry@internap.com>
2016-12-21 10:46:16 -0500

    Cleanup Day21

--- src/Year2016/Day21.py
+++ src/Year2016/Day21.py
@@ -3,10 +3,14 @@ from itertools import permutations
 import re
 from src import split_data
 
-__author__ = 'maubry'
-
 
 class Day21(object):
+    @split_data
+    def scramble(self, instructions, password):
+        for instruction in instructions:
+            password = self.scramble_instruction(instruction, password)
+        return password
+
     def brute_force_password(self, instructions, expected_password):
         possibilities = permutations(expected_password)
         valid_password = []
@@ -16,34 +20,41 @@ class Day21(object):
                 valid_password.append(''.join(password))
         return valid_password
 
-    @split_data
-    def scramble(self, instructions, password):
-        for instruction in instructions:
-            password = self.scramble_instruction(instruction, password)
-        return password
-
     def scramble_instruction(self, instruction, password):
         password = list(password)
         if instruction.startswith('swap'):
-            pos_x, pos_y = self._find_swap_position(instruction, password)
-            password[pos_x], password[pos_y] = password[pos_y], password[pos_x],
+            self.apply_swap(instruction, password)
         elif instruction.startswith('reverse'):
-            pos_x, pos_y = map(int, re.findall('(\d+)', instruction))
-            pos_y += 1
-            password[pos_x:pos_y] = password[pos_x:pos_y][::-1]
+            self.apply_reverse(instruction, password)
         elif instruction.startswith('rotate'):
-            side = self._find_rotation_side(instruction, password)
             password = deque(password)
-            password.rotate(side)
+            self.apply_rotate(instruction, password)
         elif instruction.startswith('move'):
-            pos_x, pos_y = map(int, re.findall('(\d+)', instruction))
-            letter = password[pos_x]
-            del password[pos_x]
-            if pos_y >= len(password):
-                password.append('')
-            password[pos_y] = letter + password[pos_y]
+            self.apply_move(instruction, password)
 
         return ''.join(password)
+
+    def apply_swap(self, instruction, password):
+        pos_x, pos_y = self._find_swap_position(instruction, password)
+        password[pos_x], password[pos_y] = password[pos_y], password[pos_x],
+
+    def apply_reverse(self, instruction, password):
+        pos_x, pos_y = map(int, re.findall('(\d+)', instruction))
+        pos_y += 1
+        password[pos_x:pos_y] = password[pos_x:pos_y][::-1]
+
+    def apply_rotate(self, instruction, password):
+        side = self._find_rotation_side(instruction, password)
+        password.rotate(side)
+        return password
+
+    def apply_move(self, instruction, password):
+        pos_x, pos_y = map(int, re.findall('(\d+)', instruction))
+        letter = password[pos_x]
+        del password[pos_x]
+        if pos_y >= len(password):
+            password.append('')
+        password[pos_y] = letter + password[pos_y]
 
     @staticmethod
     def _find_swap_position(instruction, password):
@@ -62,7 +73,7 @@ class Day21(object):
             return list(map(int, re.findall('(\d+)', instruction)))[0]
         else:
             letter = re.findall('letter (\w)', instruction)[0]
-            n = password.index(letter)
+            n = ''.join(password).index(letter)
             if n >= 4:
                 n += 1
             return n + 1
