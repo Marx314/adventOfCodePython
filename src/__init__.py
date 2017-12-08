ecaba9bb0372420e9a741ce59e6e791133a169c3
Marc Aubry <maubry@internap.com>
2017-12-08 00:31:06 -0500

    Day 8

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,8 @@
 def split_data(func):
     def inner(*args, **kwargs):
-        return func(args[0], args[1].split('\n'), **kwargs)
+        if len(args) == 1:
+            return func(args[0].split('\n'), **kwargs)
+        else:
+            return func(args[0], args[1].split('\n'), **kwargs)
+
     return inner
